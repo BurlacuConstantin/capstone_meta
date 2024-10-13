1123fa82f0b5f765ae7f6dc9b802bc0a77e7d204
BurlacuConstantin <burlacu_costy_9@yahoo.com.sg>
2024-10-13 14:43:35 +0200

    Initial commit for CapStone Back-end Meta Course

---
+++ littlelemon/restaurant/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
+
+
+def index(request):
+    return render(request, 'index.html', {})
+
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
